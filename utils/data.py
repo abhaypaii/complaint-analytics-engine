515511ca5c5267d79a042dac680a57900ece2c1c
Abhay Pai <abhaypai1230@gmail.com>
2026-06-25 19:18:18 +0530

    Update data.py

--- utils/data.py
+++ utils/data.py
@@ -3,17 +3,17 @@ import pandas as pd
 
 @st.cache_data
 def load_company_data():
-    return pd.read_csv("/Users/abhaypai/Library/Mobile Documents/com~apple~CloudDocs/Job stuff/Pre 2026/Projects/Portfolio Projects/RBI/Data/institutions.csv")
+    return pd.read_csv("Data/institutions.csv")
 
 @st.cache_data
 def load_trend_data():
-    return pd.read_csv("/Users/abhaypai/Library/Mobile Documents/com~apple~CloudDocs/Job stuff/Pre 2026/Projects/Portfolio Projects/RBI/Data/emerging_trends.csv")
+    return pd.read_csv("Data/emerging_trends.csv")
 
 @st.cache_data
 def load_complaint_data():
-    return pd.read_csv('/Users/abhaypai/Library/Mobile Documents/com~apple~CloudDocs/Job stuff/Pre 2026/Projects/Portfolio Projects/RBI/Data/complaints_final.csv')
+    return pd.read_csv('Data/complaints_final.csv')
 
 @st.cache_data
 def load_predicted_issue_data():
-    return pd.read_csv('/Users/abhaypai/Library/Mobile Documents/com~apple~CloudDocs/Job stuff/Pre 2026/Projects/Portfolio Projects/RBI/Data/issues_predicted.csv')
+    return pd.read_csv('Data/issues_predicted.csv')
 
