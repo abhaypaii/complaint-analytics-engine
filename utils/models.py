25c289e3c8fe7e2cd7d6abdec69324a19b3955dd
Abhay Pai <abhaypai1230@gmail.com>
2026-06-25 19:17:51 +0530

    Update models.py

--- utils/models.py
+++ utils/models.py
@@ -3,12 +3,12 @@ import joblib
 
 @st.cache_resource
 def load_models():
-    product_model = joblib.load("/Users/abhaypai/Library/Mobile Documents/com~apple~CloudDocs/Job stuff/Pre 2026/Projects/Portfolio Projects/RBI/Models/product_classifier.joblib")
-    issue_model = joblib.load("/Users/abhaypai/Library/Mobile Documents/com~apple~CloudDocs/Job stuff/Pre 2026/Projects/Portfolio Projects/RBI/Models/issue_classifier.joblib")
-    risk_model = joblib.load("/Users/abhaypai/Library/Mobile Documents/com~apple~CloudDocs/Job stuff/Pre 2026/Projects/Portfolio Projects/RBI/Models/risk_model.joblib")
-    tfidf = joblib.load("/Users/abhaypai/Library/Mobile Documents/com~apple~CloudDocs/Job stuff/Pre 2026/Projects/Portfolio Projects/RBI/Models/text_tfidf_vectorizer.joblib")
-    encoder_issue = joblib.load("/Users/abhaypai/Library/Mobile Documents/com~apple~CloudDocs/Job stuff/Pre 2026/Projects/Portfolio Projects/RBI/Models/encoder_issue.joblib")
-    encoder_product = joblib.load("/Users/abhaypai/Library/Mobile Documents/com~apple~CloudDocs/Job stuff/Pre 2026/Projects/Portfolio Projects/RBI/Models/encoder_product.joblib")
+    product_model = joblib.load("Models/product_classifier.joblib")
+    issue_model = joblib.load("Models/issue_classifier.joblib")
+    risk_model = joblib.load("Models/risk_model.joblib")
+    tfidf = joblib.load("Models/text_tfidf_vectorizer.joblib")
+    encoder_issue = joblib.load("Models/encoder_issue.joblib")
+    encoder_product = joblib.load("Models/encoder_product.joblib")
 
     return product_model, issue_model, risk_model, tfidf, encoder_issue, encoder_product
 
