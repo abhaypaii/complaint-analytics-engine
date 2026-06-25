1b838e5f9c1721912fedd35502e71fc941b2231f
Abhay Pai <abhaypai1230@gmail.com>
2026-06-25 20:28:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ st.markdown(
     """
     <style>
         section[data-testid="stSidebar"] {
-            width: 200px !important; # Set the width to your desired value
+            width: 100px !important; # Set the width to your desired value
         }
     </style>
     """,
